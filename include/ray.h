7823fabd98085a0eeda5d2b22f95069a6f6ad339
Francisco Gasca <francisco_gasca@hotmail.com>
2023-07-08 10:09:24 -0600

    Added calculateIntersection function in Ray class

--- include/ray.h
+++ include/ray.h
@@ -2,12 +2,14 @@
 #define RAY_H
 
 #include "SFML/Graphics.hpp"
+#include "wall.h"
 
 class Ray
 {
 public:
     Ray(float x, float y, float angle);
     sf::Vector2f getPosition();
+    sf::Vector2f* calculateIntersection(Wall& wall);
 private:
     float angle; // Radians
     sf::Vector2f startingPoint;
--- src/ray.cpp
+++ src/ray.cpp
@@ -16,3 +16,32 @@ void Ray::calculateDirection()
     direction.x = startingPoint.x + std::cos(angle);
     direction.y = startingPoint.y + std::sin(angle);
 }
+
+// Line-Line Intersection Approach
+sf::Vector2f* Ray::calculateIntersection(Wall& wall)
+{
+    float x1 = wall.getA().x;
+    float x2 = wall.getB().x;
+    float x3 = startingPoint.x;
+    float x4 = direction.x;
+
+    float y1 = wall.getA().y;
+    float y2 = wall.getB().y;
+    float y3 = startingPoint.y;
+    float y4 = direction.y;
+
+    float denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+
+    float t = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4) / denominator;
+    float u = (x1 - x3) * (y1 - y2) - (y1 - y3) * (x1 - x2) / denominator;
+
+    if ((t > 0 && t < 1) && (u > 0))
+    {
+        float x = x1 + t * (x2 - x1);
+        float y = y1 + t * (y2 - y1);   
+
+        return new sf::Vector2f(x, y);
+    }
+
+    return nullptr;
+}
